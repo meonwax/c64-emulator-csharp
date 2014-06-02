db0acf8e25731330286badea993bdcc2375fe8fd
kataklinger <kataklinger@gmail.com>
2014-06-02 22:01:07 +0200

    added contact and licence info to source files

--- c64_1541ii/Drive.cs
+++ c64_1541ii/Drive.cs
@@ -1,4 +1,31 @@
 ﻿
+/*
+ * 
+ * website: http://kataklinger.com/
+ * e-mail: me[at]kataklinger.com
+ * 
+ */
+
+/*
+ * 
+ * C#64 - Commodore 64 Emulator written in C#
+ * Copyright (C) 2014  Mladen Janković
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
 namespace DiskDrive
 {
 	public class Drive : Clock.ClockOp, State.IDeviceState
